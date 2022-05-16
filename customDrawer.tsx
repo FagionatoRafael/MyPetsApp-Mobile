f041006cc68be4b03caee05600e24d3a58158fc2
FagionatoRafael <67435581+FagionatoRafael@users.noreply.github.com>
2022-05-15 23:51:02 -0300

    costumizando o drawer

--- customDrawer.tsx
+++ customDrawer.tsx
@@ -3,21 +3,41 @@ import {
     DrawerItem,
     DrawerItemList,
   } from '@react-navigation/drawer';
-  
-  function CustomDrawerContent(props: any) {
-    return (
-      <DrawerContentScrollView {...props}>
-        <DrawerItemList {...props} />
-        <DrawerItem
-          label="Close drawer"
-          onPress={() => props.navigation.closeDrawer()}
-        />
-        <DrawerItem
-          label="Toggle drawer"
-          onPress={() => props.navigation.toggleDrawer()}
-        />
-      </DrawerContentScrollView>
-    );
-  }
+import { useNavigation } from '@react-navigation/native';
+import { FontAwesome } from '@expo/vector-icons'; 
+import { SafeAreaView, Text, View, Image } from 'react-native';
 
-  export default CustomDrawerContent;
+function CustomDrawerContent(props: any) {
+  const navigation = useNavigation();
+
+  return (
+    <DrawerContentScrollView {...props} style={{backgroundColor: '#5CDB95'}}>
+      <DrawerItem
+        label="Pets"
+        labelStyle={{color: '#EDF5E1'}}
+        style={{backgroundColor: '#8EE4AF'}}
+        onPress={() => {navigation.navigate('Pets') }}
+      />
+      <DrawerItem
+        label="Configurações"
+        labelStyle={{color: '#EDF5E1'}}
+        style={{backgroundColor: '#8EE4AF'}}
+        onPress={() => navigation.navigate('Configuracoes')}
+      />
+      <DrawerItem
+        label="Sair"
+        labelStyle={{color: '#EDF5E1'}}
+        style={{backgroundColor: '#FF0000'}}
+        onPress={() => navigation.navigate('Home')}
+      />
+
+      <DrawerItem
+        label="Versão 1.0.0"
+        labelStyle={{color: '#EDF5E1', marginTop: '25%'}}
+        onPress={() => alert('versão 1.0.0')}
+      />
+    </DrawerContentScrollView>
+  );
+}
+
+export default CustomDrawerContent;
