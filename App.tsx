eb621620613750da97fafe433af6ff81618042d5
FagionatoRafael <67435581+FagionatoRafael@users.noreply.github.com>
2022-07-25 18:21:17 -0300

    adicionando tela de configuração do usuario

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { AppRegistry } from 'react-native';
-import React from 'react';
+import { AppRegistry, Platform } from 'react-native';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import React, { useEffect, useRef, useState } from 'react';
 import { expo } from './app.json';
 import { NavigationContainer,  } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -38,7 +40,50 @@ type RootStackParamList = {
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+
 export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState<any>('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef<any>();
+  const responseListener = useRef<any>();
+  
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    // This listener is fired whenever a notification is received while the app is foregrounded
+    notificationListener.current = Notifications.addNotificationReceivedListener((notification: any) => {
+      setNotification(notification);
+    });
+
+    // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+
+      setTimeout(() => {
+        async () => {
+          await sendPushNotification(expoPushToken);
+        }
+      }, 5000)
+    };
+
+    
+  }, []);
+  
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home" screenOptions={{orientation: 'portrait'}}>
@@ -133,6 +178,58 @@ export default function App() {
       </Stack.Navigator>
     </NavigationContainer>      
   );
+}
+
+// Can use this function below, OR use Expo's Push Notification Tool-> https://expo.dev/notifications
+async function sendPushNotification(expoPushToken: any) {
+  const message = {
+    to: expoPushToken,
+    sound: 'default',
+    title: 'Original Title',
+    body: 'And here is the body!',
+    data: { someData: 'goes here' },
+  };
+
+  await fetch('https://exp.host/--/api/v2/push/send', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Accept-encoding': 'gzip, deflate',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(message),
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
 }
 
 AppRegistry.registerComponent(expo.name, () => App);
--- src/view/config/index.tsx
+++ src/view/config/index.tsx
@@ -1,14 +1,66 @@
-import { View, ScrollView, Text} from "react-native"
+import { View, ScrollView, Text } from "react-native"
 import CardPet from "../../components/CardPet";
-import React from "react";
+import React, { useState } from "react";
+import Container from "../../components/Container";
+import { Divider,Switch } from "react-native-paper";
+import styles from "./styles";
 
 const Config = () => {
+    const [isNoteOne, setIsNoteOne] = useState(false);
+    const [isNoteTwo, setIsNoteTwo] = useState(false);
+    const [isNoteThree, setIsNoteThree] = useState(false);
+
+    const [isMailOne, setIsMailOne] = useState(false);
+    const [isMailTwo, setIsMailTwo] = useState(false);
+    const [isMailThree, setIsMailThree] = useState(false);
+
+    const onToggleNoteOne = () => setIsNoteOne(!isNoteOne);
+    const onToggleNoteTwo = () => setIsNoteTwo(!isNoteTwo);
+    const onToggleNoteThree = () => setIsNoteThree(!isNoteThree);
+
+    const onToggleMailOne = () => setIsMailOne(!isMailOne);
+    const onToggleMailTwo = () => setIsMailTwo(!isMailTwo);
+    const onToggleMailThree = () => setIsMailThree(!isMailThree);
+    
     return (
-        <ScrollView>
-            <View >
-                <Text>Configurações</Text>
+        <Container margin={false}>
+            <View style={styles.containerTitle}>
+                <Text style={styles.title}>Configurações</Text>
             </View>
-        </ScrollView>
+
+            <View style={styles.containerConfig}>
+                <Text>Notificações</Text>
+                <Divider style={{height: 5}}/>
+                <View style={styles.containerToggle}>
+                    <Text>Pets criados/alterados</Text>
+                    <Switch value={isNoteOne} color={'#05386B'} onValueChange={onToggleNoteOne} />
+                </View>
+                <View style={styles.containerToggle}>
+                    <Text>Atividade da agenda</Text>
+                    <Switch value={isNoteTwo} color={'#05386B'} onValueChange={onToggleNoteTwo} />
+                </View>
+                <View style={styles.containerToggle}>
+                    <Text>Vacinas criados/alteradas</Text>
+                    <Switch value={isNoteThree} color={'#05386B'} onValueChange={onToggleNoteThree} />
+                </View>
+            </View>
+            <View style={styles.containerConfig}>
+                <Text>E-Mail</Text>
+                <Divider style={{height: 5}}/>
+                <View style={styles.containerToggle}>
+                    <Text>Pets criados/alterados</Text>
+                    <Switch value={isMailOne} color={'#05386B'} onValueChange={onToggleMailOne} />
+                </View>
+                <View style={styles.containerToggle}>
+                    <Text>Atividade da agenda</Text>
+                    <Switch value={isMailTwo} color={'#05386B'} onValueChange={onToggleMailTwo} />
+                </View>
+                <View style={styles.containerToggle}>
+                    <Text>Vacinas criados/alteradas</Text>
+                    <Switch value={isMailThree} color={'#05386B'} onValueChange={onToggleMailThree} />
+                </View>
+            </View>
+        </Container>
     )
 }
 
